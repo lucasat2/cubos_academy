d914a5fd9d158978957ac52db377e3a3752eb1f0
Lucas Ataide <41450845+lucasat2@users.noreply.github.com>
2024-07-17 10:48:29 -0300

    Verificar se ID é par

--- Unidade-3/Teoria/1-Introducao-ao-react/3)Exercicio/exercicio1/src/App.js
+++ Unidade-3/Teoria/1-Introducao-ao-react/3)Exercicio/exercicio1/src/App.js
@@ -1,26 +1,29 @@
 
-function Card(id, children) {
+function Card({ id, children }) {
+  
+  const idEhPar = id % 2 === 0;
+
   return (
     <div className="card">
       < img src={`https://picsum.photos/id/${id}/200/300`} alt="" />
       {children}
+      {idEhPar && <b>É par</b>} {/*Nova forma de ternario*/}
     </div>
   )
 }
 
 function App() {
-
   const cards = [
-    { id: 239, texto: 'Card 1' },
-    { id: 240, texto: 'Card 2' },
-    { id: 241, texto: 'Card 3' },
-    { id: 242, texto: 'Card 4' },
+    { id: 239,texto:'Card 1'},
+    { id: 240,texto:'Card 2'},
+    { id: 241,texto:'Card 3'},
+    { id: 242,texto:'Card 4'},
   ]
 
   return (
     <div className="App">
       {cards.map(function (card) {
-        return <Card id={card.id}>{card.texto}</Card>
+        return <Card id={card.id} key={card.id}>{card.texto}</Card>
       })}
     </div>
   );
